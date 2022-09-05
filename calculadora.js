eb8c3c2224c539e3a036c4fde4247b0d0a6174ab
Your NameCielo34 <anahileiva87@gmail.com>
2022-09-05 15:41:06 -0300

    subo la calculadora

---
+++ calculadora.js
@@ -0,0 +1,61 @@
+//calculadora
+
+
+const sumar = (num1, num2) => num1 + num2;
+
+const restar = (num1, num2) => num1 - num2;
+
+const multiplicar = (num1, num2) => num1 * num2;
+
+const dividir = (num1, num2) => num1 / num2;
+
+
+
+function mostrarValores (operacion, num1, num2){
+    switch (operacion) {
+
+        case "+":
+            console.log(sumar (num1, num2))
+        break;
+
+        case "-":
+            console.log(restar(num1, num2))
+        break;
+
+        case "*":
+            console.log(multiplicar(num1, num2))
+        break;
+
+        case "/":
+            console.log(dividir(num1, num2))
+        break;
+
+        default:
+            console.log("Operacion no valida")
+    }
+}
+
+
+let respuesta;
+
+do{
+    let num1, num2, operacion
+    do{
+        num1 = parseFloat(prompt("Ingrese numero: "))
+        num2 = parseFloat(prompt("Ingrese numero: "))
+        operacion = prompt("Ingrese una operacion: (+,-,/,*)")
+
+        if(isNaN (num1) || isNaN(num2)){
+            console.log("El numero ingresado no es valido.")
+        }
+        if(num2 === 0 && operacion == "/"){
+            console.log("No se puede dividir entre cero");
+        }
+    }while((isNaN(num1) || isNaN(num2 )|| (num2 === 0 && operacion == "/")))
+    
+    mostrarValores(operacion, num1, num2)
+
+    do{
+        respuesta = prompt("Desea ingresar otra operacion?").toLowerCase()
+    }while(respuesta != "si" && respuesta != "no")
+}while(respuesta != "no")
